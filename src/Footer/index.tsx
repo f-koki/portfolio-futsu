a2dbed17a9b9f9cb5caac1247fcf816b3e807341
f-koki <fukushimakoki0108@gmail.com>
2020-03-02 23:13:12 +0900

    feat: create portfolio foundation

---
+++ src/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="Footer">
+      <div>hoge</div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <div className="Header">
+      <div>hoge</div>
+    </div>
+  );
+};
+
+export default Header;
