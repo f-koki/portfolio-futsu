48141ddcc61f914e1d3f049bd69cb9cc10465795
f-koki <fukushimakoki0108@gmail.com>
2020-03-03 23:29:00 +0900

    feat: create main visual component

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "../Header";
+import MainVisual from "../MainVisual";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <MainVisual />
     </div>
   );
 };
---
+++ src/MainVisual/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MainVisual.css";
+
+const MainVisual: React.FC = () => {
+  return (
+    <div className="MainVisual">
+      <div>hoge</div>
+    </div>
+  );
+};
+
+export default MainVisual;
