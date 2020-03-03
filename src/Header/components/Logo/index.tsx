0687e64dc60b63b9763dc58fa3bcfb1a3db31554
f-koki <fukushimakoki0108@gmail.com>
2020-03-03 23:50:54 +0900

    feat: create logo

---
+++ src/Header/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Logo.css";
+
+const Logo: React.FC = () => {
+  return (
+    <div className="Logo">
+      <span>FUTSU TECH</span>
+    </div>
+  );
+};
+
+export default Logo;
--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css";
+import Logo from "./components/Logo";
 
 const Header: React.FC = () => {
   return (
     <div className="Header">
-      <div>hoge</div>
+      <Logo />
     </div>
   );
 };
