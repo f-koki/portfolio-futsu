f8066ac62f548285f3cc893ffa1a4f90360c886c
f-koki <fukushimakoki0108@gmail.com>
2020-03-06 00:21:32 +0900

    feat: create footer

---
+++ src/Footer/FooterSns/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./FooterSns.css";
+
+const FooterSns: React.FC = () => {
+  return (
+    <nav className="FooterSns">
+      <ul className="sns">
+        <li>Github</li>
+        <li>Twitter</li>
+        <li>note</li>
+        <li>Youtube</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default FooterSns;
--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Footer.css";
+import FooterSns from "./FooterSns";
 
 const Footer: React.FC = () => {
   return (
     <footer className="Footer">
-      <p id="copyright">© FUTSUTECH All rights reserved.</p>
+      <FooterSns />
+      <small>
+        <p id="copyright">© FUTSUTECH All rights reserved.</p>
+      </small>
     </footer>
   );
 };
