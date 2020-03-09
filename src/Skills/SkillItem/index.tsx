24c437400a3b47e92a5daba94658bf75c01a276d
f-koki <57610613+f-koki@users.noreply.github.com>
2020-03-09 18:39:17 +0900

    feat: add skill items

---
+++ src/Skills/SkillItem/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./SkillItem.css";
+
+type Props = {
+  title: string,
+  subTitle: string,
+  imagePath: string,
+  description: string,
+}
+
+const SkillItem: React.FC<Props> = ({ title, subTitle, imagePath, description }) => {
+  return (
+    <div className="SkillItem">
+      <h3>{title}</h3>
+      <p>{subTitle}</p>
+      <p><img src={imagePath} alt="skill-image" /></p>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default SkillItem;
--- src/Skills/index.tsx
+++ src/Skills/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./Skills.css";
+import SkillItem from "./SkillItem";
 
 const Skills: React.FC = () => {
   return (
     <div className="Skills">
       <h1>Skills</h1>
+      <SkillItem
+        title="プログラミング"
+        subTitle="Coding"
+        imagePath=""
+        description="プログラミングします"
+      />
+      <SkillItem
+        title="プログラミング"
+        subTitle="Coding"
+        imagePath=""
+        description="プログラミングします"
+      />
+      <SkillItem
+        title="プログラミング"
+        subTitle="Coding"
+        imagePath=""
+        description="プログラミングします"
+      />
     </div>
   );
 };
