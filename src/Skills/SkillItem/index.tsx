4cb42b5cb098eefb9dbcbbed962feb92b53fcb8d
f-koki <fukushimakoki0108@gmail.com>
2020-03-10 22:12:10 +0900

    feat: install eslint

--- src/Skills/SkillItem/index.tsx
+++ src/Skills/SkillItem/index.tsx
@@ -2,18 +2,25 @@ import React from "react";
 import "./SkillItem.css";
 
 type Props = {
-  title: string,
-  subTitle: string,
-  imagePath: string,
-  description: string,
-}
+  title: string;
+  subTitle: string;
+  imagePath: string;
+  description: string;
+};
 
-const SkillItem: React.FC<Props> = ({ title, subTitle, imagePath, description }) => {
+const SkillItem: React.FC<Props> = ({
+  title,
+  subTitle,
+  imagePath,
+  description
+}) => {
   return (
     <div className="SkillItem">
       <h3>{title}</h3>
       <p>{subTitle}</p>
-      <p><img src={imagePath} alt="skill-image" /></p>
+      <p>
+        <img src={imagePath} alt="skill" />
+      </p>
       <p>{description}</p>
     </div>
   );
