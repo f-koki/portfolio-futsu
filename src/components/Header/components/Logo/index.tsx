5a858b8574a051f2764a12eb6d75ff2bdb1c969f
f-koki <57610613+f-koki@users.noreply.github.com>
2020-03-13 12:44:14 +0900

    feat: not change a tag color and unset underline

--- src/components/Header/components/Logo/index.tsx
+++ src/components/Header/components/Logo/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Logo.css";
+import { Link } from "react-router-dom";
 
 const Logo: React.FC = () => {
   return (
     <div className="Logo">
-      <span>FUTSU TECH</span>
+      <span><Link to="/">FUTSU TECH</Link></span>
     </div>
   );
 };
