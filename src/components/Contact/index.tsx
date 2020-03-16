723f3468c7b21c2aceede72b8e5223fb64b796ec
f-koki <57610613+f-koki@users.noreply.github.com>
2020-03-16 21:30:04 +0900

    feat: update about and skills

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./Contact.css";
 
 const Contact: React.FC = () => {
+
+  // TODO: firebaseでメールフォームを作成
+  const onSubmit = () => {
+  }
+
   return (
     <div className="Contact">
       <h1>Contact</h1>
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -7,22 +7,22 @@ const Skills: React.FC = () => {
     <div className="Skills">
       <h1>Skills</h1>
       <SkillItem
-        title="話し相手"
-        subTitle="Talking"
+        title="大喜利"
+        subTitle="Oogiri"
         imagePath=""
-        description="30分間、無料で話し相手になります。決して否定しません。トークテーマは本当になんでもOKです。言語化のお手伝いをします。お気軽にご活用ください。"
+        description="お題をいただけたら、答えます。面白さの保証はしません。あんまり送ってこないでください。"
       />
       <SkillItem
-        title="プログラミング"
-        subTitle="Coding"
+        title="ピアノ"
+        subTitle="Piano"
         imagePath=""
-        description="現役フリーランスのエンジニアが、プログラミングや仕事の取り方の相談に乗ります。「話し相手」なら無料ですむので最初はそちらをオススメします。"
+        description="リクエストいただいた曲を、ピアノで弾きます。素人に毛が生えた程度なので、練習にめちゃくちゃ時間がかかります。気長にお待ち下さい。"
       />
       <SkillItem
         title="ライフハック"
         subTitle="Lifehack"
         imagePath=""
-        description="生粋のライフハックマニアが日々のコンサルティングをします。「話し相手」なら無料ですむので最初はそちらをオススメします。"
+        description="ライフハックが好きです。家電は声で操作してます。これに関してとくにサービスとかはありません。"
         isLast
       />
     </div>
