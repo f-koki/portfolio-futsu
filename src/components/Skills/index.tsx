73e455ddb22d9ba22f6a20ce03251c6c05c1a433
f-koki <fukushimakoki0108@gmail.com>
2020-03-17 22:50:56 +0900

    feat: add skills

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -16,14 +16,39 @@ const Skills: React.FC = () => {
         title="ピアノ"
         subTitle="Piano"
         imagePath=""
-        description="リクエストいただいた曲を、ピアノで弾きます。素人に毛が生えた程度なので、練習にめちゃくちゃ時間がかかります。気長にお待ち下さい。"
+        description="聴いたことのある曲をピアノで弾けます。リクエストいただけたら、弾きます。練習にめちゃくちゃ時間がかかりますので、気長にお待ち下さい。"
       />
       <SkillItem
         title="ライフハック"
         subTitle="Lifehack"
         imagePath=""
-        description="ライフハックが好きです。家電は声で操作してます。これに関してとくにサービスとかはありません。"
+        description="一番お金を使っているのはライフハックです。これに関してとくにサービスとかはありません。相談とかは乗ります。"
         isLast
+      />
+      <SkillItem
+        title="スマブラ"
+        subTitle="Smash Bros"
+        imagePath=""
+        description="人口上位3％のレベルです。興味ある人は、対戦しましょう。"
+      />
+      <SkillItem
+        title="英語"
+        subTitle="English"
+        imagePath=""
+        description="英語を使う現場で仕事をしていたので、ほどほどに喋れます。"
+      />
+      <SkillItem
+        title="プログラミング"
+        subTitle="Programing"
+        imagePath=""
+        description="WebサイトやWebサービスを作ります。サイト制作依頼は、こちらのフォームからお願いいたします。"
+        isLast
+      />
+      <SkillItem
+        title="言語化"
+        subTitle="Verbalization"
+        imagePath=""
+        description="心の中のもやもやを言語化します。"
       />
     </div>
   );
