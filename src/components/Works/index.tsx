266d2d7948401fd339277803d767716d07bc8024
f-koki <57610613+f-koki@users.noreply.github.com>
2020-03-23 19:24:50 +0900

    note: add TODO comment

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -4,7 +4,9 @@ import "./Works.css";
 const Works: React.FC = () => {
   return (
     <div className="Works">
-      <h1>Works</h1>      
+      <h1>Works</h1>
+      {/* TODO: WorkItem Componentを作成する */}
+      {/* TODO: Programming,Piano,Mentoringでわける */}
     </div>
   );
 };
