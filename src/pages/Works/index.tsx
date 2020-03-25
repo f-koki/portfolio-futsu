3e006e10fa36f0b7f8980bc0b3a5bb0d6d751166
f-koki <fukushimakoki0108@gmail.com>
2020-03-25 12:00:00 +0900

    feat: add works tabbar

--- src/pages/Works/index.tsx
+++ src/pages/Works/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Works.css";
+import TabBar, { TabBarLink } from "../../components/TabBar";
 
 const Works: React.FC = () => {
+  const worksTab: TabBarLink[] = [
+    { path: "/", message: "Piano" },
+    { path: "/", message: "Programming" },
+    { path: "/", message: "Mentoring" }
+  ];
+
   return (
     <div className="Works">
       <h1>Works</h1>
       {/* TODO: WorkItem Componentを作成する */}
+      <TabBar tabBarLinks={worksTab} />
       {/* TODO: Programming,Piano,Mentoringでわける */}
     </div>
   );
