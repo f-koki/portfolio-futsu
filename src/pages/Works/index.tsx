77aa21d3c131eb6f58dd8ea5d199a65f9912ce97
f-koki <fukushimakoki0108@gmail.com>
2020-03-26 22:49:37 +0900

    feat: update work paths

--- src/pages/Works/index.tsx
+++ src/pages/Works/index.tsx
@@ -4,9 +4,9 @@ import TabBar, { TabBarLink } from "../../components/TabBar";
 
 const Works: React.FC = () => {
   const worksTab: TabBarLink[] = [
-    { path: "/", message: "Piano" },
-    { path: "/", message: "Programming" },
-    { path: "/", message: "Mentoring" }
+    { path: "/works/piano", message: "Piano" },
+    { path: "/works/programming", message: "Programming" },
+    { path: "/works/lifehack", message: "Lifehack" }
   ];
 
   return (
