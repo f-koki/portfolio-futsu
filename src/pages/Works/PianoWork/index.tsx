70ac660eb94bbe22d5aaddd45e6119e05051cb90
f-koki <fukushimakoki0108@gmail.com>
2020-03-27 23:54:42 +0900

    feat: create work component

--- src/pages/Works/PianoWork/index.tsx
+++ src/pages/Works/PianoWork/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./PianoWork.css";
+import Work from "../component/Work";
 
 const PianoWork: React.FC = () => {
   return (
     <div className="PianoWork">
       {/* TODO: Workを追加 */}
-      <div>piano</div>
+      <Work />
     </div>
   );
 };
---
+++ src/pages/Works/component/Work/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Work.css";
+
+const Work: React.FC = () => {
+  return (
+    <div className="Work">
+      <h1>Title</h1>
+    </div>
+  );
+};
+
+export default Work;
