7c7471ba43c970d1cd672451b7e66e5a5e64307c
f-koki <fukushimakoki0108@gmail.com>
2020-04-03 23:35:01 +0900

    feat: create form (not working)

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -2,14 +2,25 @@ import React from "react";
 import "./Contact.css";
 
 const Contact: React.FC = () => {
-
   // TODO: firebaseでメールフォームを作成
-  const onSubmit = () => {
-  }
+  const onSubmit = () => {};
 
   return (
     <div className="Contact">
       <h1>Contact</h1>
+      <form action="post">
+        <div>お名前</div>
+        <input type="text" />
+        <div>メールアドレス</div>
+        <input type="text" />
+        <div>電話番号</div>
+        <input type="text" />
+        <div>お問い合わせ内容</div>
+        <textarea name="message" cols={30} rows={10}></textarea>
+        <div>
+          <input type="submit" value="内容を送信する" />
+        </div>
+      </form>
     </div>
   );
 };
