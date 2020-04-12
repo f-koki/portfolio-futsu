4a4267ee9c07271ca6d7b2938debaf0a8c1e6df7
f-koki <fukushimakoki0108@gmail.com>
2020-04-12 23:36:53 +0900

    feat

--- src/pages/Works/ProgrammingWork/index.tsx
+++ src/pages/Works/ProgrammingWork/index.tsx
@@ -7,13 +7,18 @@ const ProgrammingWork: React.FC = () => {
     <div className="ProgrammingWork workList">
       <Work
         title="ECサイトのサーバーサイド・フロントエンド構築"
-        description="月間3億アクセスある某大手ECサイトのサーバーサイド/フロントエンドエンジニアを担当。"
-        skills={["Java", "JavaScript", "CSS", "Git"]}
+        description="月間3億アクセスされるECサイトのサーバーサイド/フロントエンド。設計、実装、テスト、ソースレビューまで担当。"
+        skills={["Java", "JavaScript", "CSS", "Git", "HTML"]}
       ></Work>
       <Work
-        title="テスト効率化ツールの作成"
-        description="某大手ECサイトでテスト効率化のChrome拡張機能を作成。"
-        skills={["JavaScript", "Chrome Extension", "CSS"]}
+        title="ECサイトテスト効率化ツール作成"
+        description="チームの開発効率を上げるためのChrome拡張機能を作成。テスト時間を20％減させることに成功。"
+        skills={["JavaScript", "Chrome Extension", "CSS", "HTML", "Git"]}
+      ></Work>
+      <Work
+        title="東京都コロナ対策サイトへの貢献"
+        description="オープンソースへの貢献。CSS修正やドキュメント整備など、本業の合間で着手。"
+        skills={["Vue", "Nuxt", "SCSS", "Git"]}
       ></Work>
     </div>
   );
