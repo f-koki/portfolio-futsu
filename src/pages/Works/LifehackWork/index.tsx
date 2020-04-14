dded416908d16f0df97885209669766335c33226
f-koki <57610613+f-koki@users.noreply.github.com>
2020-04-14 21:20:23 +0900

    feat

--- src/pages/Works/LifehackWork/index.tsx
+++ src/pages/Works/LifehackWork/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./LifehackWork.css";
+import Work from "../component/Work";
 
 const LifehackWork: React.FC = () => {
   return (
     <div className="LifehackWork workList">
-      {/* TODO: Workを追加 */}
-      <div>lifehack</div>
+      <Work
+        title="Lifehack"
+        description="ライフハックのコンサルに乗っています。"
+      />
     </div>
   );
 };
