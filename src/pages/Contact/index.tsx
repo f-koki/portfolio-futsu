824618ef4a6dc6ad76e686bd3b975d2007e92b4e
f-koki <fukushimakoki0108@gmail.com>
2020-04-25 20:07:31 +0900

    feat

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -3,12 +3,16 @@ import "./Contact.css";
 
 const Contact: React.FC = () => {
   // TODO: firebaseでメールフォームを作成
-  const onSubmit = () => {};
+  // const onSubmit = () => {};
 
   return (
     <div className="Contact">
       <h1>Contact</h1>
-      <form action="post">
+      <p>ご連絡はTwiterのDMからどうぞ。</p>
+      <p>
+        Twitter: <a href="https://twitter.com/futsu_tech">@futsu_tech</a>
+      </p>
+      {/* <form action="post">
         <div>スイッチのフレコ</div>
         <input type="text" />
         <div>ひとこと</div>
@@ -16,7 +20,7 @@ const Contact: React.FC = () => {
         <div>
           <input type="submit" value="内容を送信する" />
         </div>
-      </form>
+      </form> */}
     </div>
   );
 };
