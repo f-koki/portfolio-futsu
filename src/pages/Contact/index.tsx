d2282bb6579e46d651eebea4339e4df103f7fce8
f-koki <fukushimakoki0108@gmail.com>
2020-04-27 22:31:59 +0900

    feat

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -12,15 +12,6 @@ const Contact: React.FC = () => {
       <p>
         Twitter: <a href="https://twitter.com/futsu_tech">@futsu_tech</a>
       </p>
-      {/* <form action="post">
-        <div>スイッチのフレコ</div>
-        <input type="text" />
-        <div>ひとこと</div>
-        <textarea name="message" cols={30} rows={10}></textarea>
-        <div>
-          <input type="submit" value="内容を送信する" />
-        </div>
-      </form> */}
     </div>
   );
 };
