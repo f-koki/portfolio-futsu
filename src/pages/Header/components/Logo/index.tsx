748b0eef0b9933e34e31466db26c2a120639408f
f-koki <fukushimakoki0108@gmail.com>
2020-05-02 12:00:00 +0900

    feaat

--- src/pages/Header/components/Logo/index.tsx
+++ src/pages/Header/components/Logo/index.tsx
@@ -5,7 +5,12 @@ import { Link } from "react-router-dom";
 const Logo: React.FC = () => {
   return (
     <div className="Logo">
-      <span><Link to="/">FUTSU TECH</Link></span>
+      <span>
+        <Link className="title" to="/">
+          FUTSU TECH
+        </Link>
+        <div className="subtitle">ENGINEER & KEYBOARDIST & LIFEHACKER</div>
+      </span>
     </div>
   );
 };
