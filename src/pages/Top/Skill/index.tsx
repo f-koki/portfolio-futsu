6978f8854d815b209a5769d3287d60947f993a1e
f-koki <57610613+f-koki@users.noreply.github.com>
2020-06-15 21:19:27 +0900

    feat

--- src/pages/Top/Skill/index.tsx
+++ src/pages/Top/Skill/index.tsx
@@ -25,25 +25,6 @@ const Skill: React.FC = () => {
         description="暮らしに抱えるご不便を、ライフハックで解決します。noteをご覧ください。"
         isLast
       />
-      <SkillItem
-        title="スマブラ"
-        subTitle="Super Smash Bros"
-        imagePath=""
-        description="ロボットでVIPです。闘いたい方は、こちらのフレコをご登録ください。"
-      />
-      <SkillItem
-        title="英語"
-        subTitle="English"
-        imagePath=""
-        description="英語を使う現場で仕事をしていたので、ほどほどに喋れます。"
-      />
-      <SkillItem
-        title="大喜利"
-        subTitle="Oogiri"
-        imagePath=""
-        description="お題をいただけたら、答えます。面白さの保証はしません。あんまり送ってこないでください。"
-        isLast
-      />
     </div>
   );
 };
