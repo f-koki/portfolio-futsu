71a71b5a9bff4e4e325c5efeb4785d43d57916dc
f-koki <fukushimakoki0108@gmail.com>
2020-07-02 23:36:30 +0900

    feat: remove works tab change

--- src/pages/Works/index.tsx
+++ src/pages/Works/index.tsx
@@ -6,45 +6,10 @@ import ProgrammingWork from "./ProgrammingWork";
 import classNames from "classnames";
 
 const Works: React.FC = () => {
-  const [tab, setTab] = useState("programming");
-
   return (
     <div className="Works">
       <h1>Works</h1>
-      <div>
-        <ul className="tabList">
-          <li
-            className={classNames(
-              "tab programming",
-              tab === "programming" ? "active" : undefined
-            )}
-            onClick={() => setTab("programming")}
-          >
-            PROGRAMMING
-          </li>
-          <li
-            className={classNames(
-              "tab piano",
-              tab === "piano" ? "active" : undefined
-            )}
-            onClick={() => setTab("piano")}
-          >
-            KEYBOARD
-          </li>
-          <li
-            className={classNames(
-              "tab lifehack",
-              tab === "lifehack" ? "active" : undefined
-            )}
-            onClick={() => setTab("lifehack")}
-          >
-            <span>LIFEHACK</span>
-          </li>
-        </ul>
-      </div>
-      {tab === "programming" && <ProgrammingWork />}
-      {tab === "piano" && <PianoWork />}
-      {tab === "lifehack" && <LifehackWork />}
+      <ProgrammingWork />
     </div>
   );
 };
